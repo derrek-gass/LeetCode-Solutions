8910a8f473f88df573fc9c9593905520cf4db850
derrek-gass <dardcore@gmail.com>
2022-02-20 16:30:17 -0800

    twosum

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -11,7 +11,14 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    
+  let length = nums.length;
+    for (i = 0; i < length; i++) {
+      for (j = i+1; j < length; j++) {
+        if (nums[i] + nums[j] === target) {
+          return [i, j];
+        }
+       }
+    }
 };
 // @lc code=end
 
