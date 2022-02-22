ee2119b54e25877c56d557db1e7e96ff02b689bf
derrek-gass <dardcore@gmail.com>
2022-02-22 07:56:41 -0800

    Iterative inorder tree traversal using stack

--- 94.binary-tree-inorder-traversal.java
+++ 94.binary-tree-inorder-traversal.java
@@ -23,16 +23,18 @@
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        helper(root, result);
-        return result;
-    }
-
-    public void helper(TreeNode root, List<Integer> result) {
-        if (root != null) {
-            helper(root.left, result);
-            result.add(root.val);
-            helper(root.right, result);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
         }
+        return result;
     }
 }
 // @lc code=end
