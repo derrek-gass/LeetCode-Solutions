88c41fd6afd96e63be4d85bbcc91a63ecdbce10c
derrek-gass <dardcore@gmail.com>
2023-05-31 11:43:27 -0700

    test

---
+++ 9.palindrome-number.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=9 lang=java
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPalindrome(int x) {
+        int reversed = 0;
+        int original = x;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
+// @lc code=end
