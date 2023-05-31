0a0b54750e4d4f7b5bf774eeea2ec41788a23fb7
derrek-gass <dardcore@gmail.com>
2023-05-31 11:58:54 -0700

    roman

--- 13.roman-to-integer.java
+++ 13.roman-to-integer.java
@@ -7,7 +7,29 @@
 // @lc code=start
 class Solution {
     public int romanToInt(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
 
+        int result = 0;
+        int prevValue = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int curValue = romanMap.get(c);
+            if (curValue < prevValue) {
+                result -= curValue;
+            } else {
+                result += curValue;
+            }
+            prevValue = curValue;
+        }
+
+        return result;
     }
 }
 // @lc code=end
