8a5f1d13ee3145854b2d3d50c8e2f20dbb9e0f1a
derrekgass <dardcore@gmail.com>
2024-02-16 18:35:04 -0800

    update

--- rust-solutions/src/evaluate_reverse_polish_notation_150.rs
+++ rust-solutions/src/evaluate_reverse_polish_notation_150.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 /*
  * @lc app=leetcode id=150 lang=rust
  *
@@ -8,37 +9,30 @@
 impl Solution {
     pub fn eval_rpn(tokens: Vec<String>) -> i32 {
         let mut stack: Vec<i32> = Vec::new();
-        for token in tokens {
-            match token.as_str() {
-                "+" => {
-                    let b: i32 = stack.pop().unwrap();
-                    let a = stack.pop().unwrap();
-                    stack.push(a + b);
+
+        for i in tokens {
+
+            let mut val = i.as_str();
+
+            if ["+", "-", "*", "/"].contains(&val) {
+                let mut x = stack.pop().unwrap();
+                let mut y = stack.pop().unwrap();
+
+                match val {
+                    "+" => stack.push(x + y),
+                    "-" => stack.push(y - x),
+                    "*" => stack.push(x * y),
+                    "/" => stack.push(y / x),
+                    _ => continue
                 }
-                "-" => {
-                    let b = stack.pop().unwrap();
-                    let a = stack.pop().unwrap();
-                    stack.push(a - b);
-                }
-                "*" => {
-                    let b = stack.pop().unwrap();
-                    let a = stack.pop().unwrap();
-                    stack.push(a * b);
-                }
-                "/" => {
-                    let b = stack.pop().unwrap();
-                    let a = stack.pop().unwrap();
-                    stack.push(a / b);
-                }
-                _ => {
-                    stack.push(token.parse::<i32>().unwrap());
-                }
+            } else {
+                stack.push(i.parse::<i32>().unwrap());
             }
         }
-        stack.pop().unwrap()
+
+        return stack.pop().unwrap();
     }
 }
-
     // fn main() {
     //     assert_eq!(eval_rpn(vec!["2", "1", "+", "3", "*"].iter().map(|s| s.to_string()).collect()), 9, "Wrong result");
     //     assert_eq!(eval_rpn(vec!["2", "1", "+", "3", "*"].iter().map(|s| s.to_string()).collect()), 8, "Wrong result");
--- rust-solutions/src/main.rs
+++ rust-solutions/src/main.rs
@@ -1,4 +1,5 @@
 mod evaluate_reverse_polish_notation_150;
+mod generate_parentheses_22;
 
 fn main() {
     println!("Hello, world!");
